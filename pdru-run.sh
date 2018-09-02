463931f08fb2f1c8ff17f54f6ebe3c324e7249b4
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-09-02 11:28:14 +0800

    Added env.sample.

--- pdru-run.sh
+++ pdru-run.sh
@@ -2,6 +2,6 @@
 docker volume create pdrudata
 docker run --rm --name pdru -d \
         -h pdru \
-	--env-file=env.secret \
+	--env-file=beyondbroadcast.com.secret \
         -v pdrudata:/etc/rspamd/local.d \
         siuyin/pdru
