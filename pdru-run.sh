81313c9cfbe072fdfa211ed5cede6b05811558b0
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-04-16 11:21:58 +0800

    fix: update pdru-run with actual config

--- pdru-run.sh
+++ pdru-run.sh
@@ -2,6 +2,11 @@
 docker volume create pdrudata
 docker run --rm --name pdru -d \
         -h pdru \
-	--env-file=beyondbroadcast.com.secret \
+        --env-file=beyondbroadcast.com.secret \
         -v pdrudata:/etc/rspamd/local.d \
+        -v /home/siuyin/pdru/mail:/var/mail/domains \
+        -v $(pwd)/go.beyondbroadcast.com/fullchain.pem:/etc/dovecot/private/dovecot.pem \
+        -v $(pwd)/go.beyondbroadcast.com/privkey.pem:/etc/dovecot/private/dovecot.key \
+        -p 25:25 \
+        -p 143:143 \
         siuyin/pdru
